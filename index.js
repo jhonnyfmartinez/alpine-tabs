8a1b99ca7c86664ad37842f38d791357d1d7d6b1
Jhonny Martinez <jmartinez@ideabti.com>
2020-03-08 00:13:08 +1100

    Breaking Bad quotes fetching added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function tabs() {
   return {
     current: 'Colors',
-    list: ['Colors', 'Movies', 'About'],
+    items: ['Colors', 'Quotes', 'About'],
     changeTab(tab) {
       this.current = tab;
     }
@@ -23,6 +23,26 @@ function colors() {
     },
     getCurrentColor() {
       return this.list.find(c => c.name === this.selected);
+    }
+  };
+}
+
+function quotes() {
+  return {
+    quote: {},
+    error: null,
+    loading: false,
+    async fetchQuote() {
+      const apiUrl = 'https://breaking-bad-quotes.herokuapp.com/v1/quotes';
+      this.loading = true;
+      try {
+        const result = await fetch(apiUrl);
+        const [quote] = await result.json();
+        this.quote = quote;
+      } catch (error) {
+        this.error = 'Oops. Something went wrong.';
+      }
+      this.loading = false;
     }
   };
 }
